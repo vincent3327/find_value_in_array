95c1fadcef92a400ef10db1eb0208c91b9ba0cfe
vincent_1126 <359659848@qq.com>
2020-07-28 19:28:25 +0800

    solve wantvalue unexisted

--- find.c
+++ find.c
@@ -10,22 +10,26 @@ int array[] = {1,2,33,44,23,456,77,5,8,9,0,123,543};
 int findval(int val,int length)
 {
 	int pos,tmp;
+	unsigned char flag = 0;
 	for(int i=0;i<length;i++)
 	{
 		tmp = val^array[i];
 		if(0==tmp)
 		{	//printf(i)
-			pos = i ;
+			pos = i;
+			flag = 1;
 			break;
 		}
 	}
+	if(1==flag)
 	return pos;
+	else
+	return -1;
 }
 
 int main()
 {
-	int val = 456;
+	int val = 4356;
 	
 	printf("the value:%d is at %dth positon in array\n",val,findval(val,DIM(array)));
 }
-ccc
